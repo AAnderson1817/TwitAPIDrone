3668c3bc3a8fdaaa1da2f97157b0ca17631bf1a8
Andy Anderson <andyanderson1818@gmail.com>
2017-05-30 20:29:08 -0700

    Added app and config files and populated config file with access tokens

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: 'YxUP9oF76P0jiR1QswtwZbV1p',
+  consumer_secret: 'CeGn8r9BYokWMShIrAxvQiYVLNZGOtfsCSwHwEeXjJvJX2ExIy',
+  access_token_key: '869751139268386816-d4QpGbzauMIaW3PiNm48YuslIjb0xnc',
+  access_token_secret: 'QuP2qneWzomWPkFz4HPVekrvAMYBrvLODJ2eksfKcOhOs'
+}
